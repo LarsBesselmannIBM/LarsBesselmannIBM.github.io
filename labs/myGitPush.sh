a247d92626b33394654077e122d6fbe9d68a28e3
LarsBesselmannIBM <Lars.Besselmann@de.ibm.com>
2022-02-18 09:17:25 +0100

    Update files

--- labs/myGitPush.sh
+++ labs/myGitPush.sh
@@ -1,3 +1,6 @@
+echo "add files"
 git add --all
-git commit -m "Rename files"
-git push https://LarsBesselmannIBM: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@github.com/LarsBesselmannIBM/LarsBesselmannIBM.github.io
+echo "commit files"
+git commit -m "Update files"
+echo "push files"
+git push "https://LarsBesselmannIBM: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@github.com/LarsBesselmannIBM/LarsBesselmannIBM.github.io"
