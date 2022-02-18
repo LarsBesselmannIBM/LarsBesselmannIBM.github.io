fac64e5ea252f51ba30bb343ca613f62b8606699
Lars Besselmann <32673634+LarsBesselmannIBM@users.noreply.github.com>
2022-02-18 09:29:25 +0100

    Delete myGitPush.sh

--- myGitPush.sh
+++
@@ -1,6 +0,0 @@
-echo "add files"
-git add --all
-echo "commit files"
-git commit -m "Update files"
-echo "push files"
-git push https://LarsBesselmannIBM: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@github.com/LarsBesselmannIBM/LarsBesselmannIBM.github.io
