e99a2ba8cb9ff0b32baafe95b619c530a3e0af67
LarsBesselmannIBM <Lars.Besselmann@de.ibm.com>
2022-02-22 11:39:21 +0100

    Update files

--- labs/myGitPush.sh
+++
@@ -1,6 +0,0 @@
-echo "add files"
-git add --all
-echo "commit files"
-git commit -m "Update files"
-echo "push files"
-git push "https://LarsBesselmannIBM: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@github.com/LarsBesselmannIBM/LarsBesselmannIBM.github.io"
