1eecf10967c41a482563102a22ff0957330fa013
LarsBesselmannIBM <Lars.Besselmann@de.ibm.com>
2022-04-25 16:34:00 +0200

    Update files

--- labs/WSA/setWSA_IP.sh
+++ labs/WSA/setWSA_IP.sh
@@ -1,3 +1,8 @@
-echo "Try to set WebSphere Automation target address: "
-sudo sed -i 's/10.99.99.23/10.139.195.190/g' /etc/hosts
-cat /etc/hosts
+export WSA_ADDRESS= 10.139.195.190
+echo "Try to set WebSphere Automation target address: "$WSA_ADDRESS
+if [[ "$WSA_ADDRESS" != "" ]]; then
+  sudo sed -i 's/10.99.98.223/'$WSA_ADDRESS'/g' /etc/hosts
+  cat /etc/hosts
+else
+  echo "No IP address inserted"
+fi
